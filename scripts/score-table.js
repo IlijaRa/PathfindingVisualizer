bec9db5caf9f2e667e0034a93604cd7ec7733fc3
IlijaRa <rakocevicilija7@gmail.com>
2023-02-21 23:38:54 +0100

    html-table-to-csv

--- scripts/score-table.js
+++ scripts/score-table.js
@@ -68,79 +68,6 @@ function addScoreToTable(noPathNodes, noSearchedNodes, executionTime){
     tableRow.appendChild(clearButton);
 
     table.appendChild(tableRow);
-    // return;
-    // let scorePopulationgElement = document.getElementById("score-populating-part");
-    // //<div id="row[someIndex]" class="score-table-row"></div>
-    // let newRowElement = document.createElement('div');
-    // let last_index = scorePopulationgElement.childNodes.length;
-    // newRowElement.setAttribute('id', 'row' + (last_index + 1));
-    // newRowElement.classList.add('score-table-row');
-
-    // //<div class="scores-col alg-name-col">[someValue]</div>
-    // let algNameElement = document.createElement('div');
-    // algNameElement.classList.add('scores-col', 'alg-name-col');
-    // algNameElement.innerHTML = ACTIVE_ALGORITHM;
-    // newRowElement.appendChild(algNameElement);
-
-    // // <div class="scores-col path-legth-col">33</div>
-    // let pathLengthElement = document.createElement('div');
-    // pathLengthElement.classList.add('scores-col', 'path-legth-col');
-    // pathLengthElement.innerHTML = noPathNodes;
-    // newRowElement.appendChild(pathLengthElement);
-
-    // // <div class="scores-col searched-nodes-col">465</div>
-    // let searchedNodesElement = document.createElement('div');
-    // searchedNodesElement.classList.add('scores-col', 'searched-nodes-col');
-    // searchedNodesElement.innerHTML = noSearchedNodes;
-    // newRowElement.appendChild(searchedNodesElement);
-
-    // // <div class="scores-col execution-time-col">14500.43</div>
-    // let executionTimeElement = document.createElement('div');
-    // executionTimeElement.classList.add('scores-col', 'execution-time-col');
-    // executionTimeElement.innerHTML = executionTime;
-    // newRowElement.appendChild(executionTimeElement);
-
-    // // <div class="scores-col canvas-width-col">50</div>
-    // let canvasWidthElement = document.createElement('div');
-    // canvasWidthElement.classList.add('scores-col', 'canvas-width-col');
-    // canvasWidthElement.innerHTML = WIDTH;
-    // newRowElement.appendChild(canvasWidthElement);
-
-    // // <div class="scores-col execution-speed-col">450</div>
-    // let executionSpeedElement = document.createElement('div');
-    // executionSpeedElement.classList.add('scores-col', 'execution-speed-col');
-    // executionSpeedElement.innerHTML = INITIAL_SPEED_VALUE;
-    // newRowElement.appendChild(executionSpeedElement);
-
-    // // <div class="scores-col heuristic-col">/</div>
-    // let heuristicElement = document.createElement('div');
-    // heuristicElement.classList.add('scores-col', 'heuristic-col');
-    // if(ACTIVE_ALGORITHM == "Dijkstra" || ACTIVE_ALGORITHM == "Greedy BFS" || ACTIVE_ALGORITHM == "A* - Astar")
-    //     heuristicElement.innerHTML = CHOSEN_HEURISTIC;    
-    // else
-    //     heuristicElement.innerHTML = "/";
-    // newRowElement.appendChild(heuristicElement);
-
-    // // <div class="scores-col details-level-col">High</div>
-    // let detailsLevelElement = document.createElement('div');
-    // detailsLevelElement.classList.add('scores-col', 'details-level-col');
-    // detailsLevelElement.innerHTML = LEVEL_OF_DETAILS;
-    // newRowElement.appendChild(detailsLevelElement);
-
-    // // <div class="scores-col clear-content-col"><button class="score-table-clear-button">Clear</button></div>
-    // let clearContentElement = document.createElement('div');
-    // clearContentElement.classList.add('scores-col', 'clear-content-col');
-    // let clearButton = document.createElement('button');
-    // clearButton.classList.add('score-table-clear-button');
-    // clearButton.innerHTML = 'Clear';
-    // clearContentElement.appendChild(clearButton);
-    // clearContentElement.addEventListener('click', function(e){
-    //     if(confirm('Are you sure that you want to delete this record?'))
-    //         newRowElement.remove();
-    // })
-
-    // newRowElement.appendChild(clearContentElement);
-    // scorePopulationgElement.appendChild(newRowElement);
 }
 
 document.getElementById("score-table-clear-all-button").addEventListener('click', function(e){
@@ -155,3 +82,25 @@ document.getElementById("score-table-clear-all-button").addEventListener('click'
         });
     }
 });
+
+function exportTableToCSV() {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+    
+    for (var i = 0; i < rows.length; i++) {
+      var row = [], cols = rows[i].querySelectorAll("td, th");
+      
+      for (var j = 0; j < cols.length; j++) 
+        row.push(cols[j].textContent);
+      
+      csv.push(row.join(","));
+    }
+    
+    var csvFile = new Blob([csv.join("\n")], {type: "text/csv"});
+    var downloadLink = document.createElement("a");
+    downloadLink.download = "table.csv";
+    downloadLink.href = URL.createObjectURL(csvFile);
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+  }
